1466ae5a0aae4315f54f645ef8013a1bff5564a1
kinocastle <2238932729@qq.com>
2016-08-15 16:00:33 +0800

    Add files via upload

--- tool-mk.c
+++ tool-mk.c
@@ -4,21 +4,32 @@
 #include <unistd.h>
 #include <malloc.h>
 #include <string.h>
-#define MAXSIZE 10000*100
+#define MAXSIZE 10000
 
 int main(int argc, char *argv[]) {
-	char *emit, *fname = argv[1];
+	char *src, *emit, *fname = NULL;
+	int reset = 0;
+	FILE *fp;
+	if(argc < 2) { printf("error!\n"); exit(-1); }
+	for(int i = 1; i < argc; i++) {
+		if(!strcmp(argv[i], "-r")) {
+			reset = 1;
+		} else {
+			fname = argv[i];
+		}
+	}
+	if(!fname) { printf("error!\n"); exit(-1); }
+	if(!(fp = fopen(fname, "r"))) { printf("error!\n"); exit(-1); }
+	
 	{ //read
-		FILE *fp = fopen(fname, "r");
-		//p = (char*)malloc(MAXSIZE * sizeof(char));
 		fseek(fp, 0, SEEK_END);
 		int fsize = ftell(fp);
 		fseek(fp, 0, SEEK_SET);
-		char *p = (char*)malloc(fsize * sizeof(char));
-		char *e = emit = (char*)malloc(MAXSIZE * sizeof(char));
+		char *p = src = (char*)malloc(fsize * sizeof(char));
+		char *e = emit = (char*)malloc((fsize + MAXSIZE) * sizeof(char));
+		emit[0] = '\0';
 		
-		//{ int i = fread(p, sizeof(char), MAXSIZE, fp); p[i] = '\0'; }//printf("%s",p);
-		{ int i = fread(p, sizeof(char), fsize, fp); p[i] = '\0'; }//printf("%s",p);
+		{ int i = fread(p, sizeof(char), fsize, fp); p[i] = '\0'; }
 		fclose(fp);
 		
 		char buf[100], *str = "error";
@@ -27,10 +38,14 @@ int main(int argc, char *argv[]) {
 			if(!strncmp(p, str, len)) {
 				p += len;
 				while(*p >= '0' && *p <= '9') p++;
-				sprintf(buf, "%s%d", str, ++count);//printf("%s\n",buf);
+				if(reset) {
+					sprintf(buf, "%s", str);
+				} else {
+					sprintf(buf, "%s%d", str, ++count);
+				}
 				strcat(emit, buf);
 				e += strlen(buf);
-				//sprintf(emit, "%s%s%d", emit, str, ++count); //效率低
+				//sprintf(emit, "%s%s%d", emit, str, ++count); //效率低，废弃
 				//e = emit + strlen(emit);
 			} else {
 				*e++ = *p++;
@@ -38,31 +53,34 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+	
 	{ //write
-		/*打印*/
+		//打印
 		//printf("%s", emit);
 		
-		/*覆盖原文件*/
-		//fp = fopen(fname, "w");
-		//fwrite(emit, sizeof(char), strlen(emit), fp);
-		
-		/*不覆盖原文件*/
-		int i;
-		char buf[100], *fe;
-		if(fe = strrchr(fname, '.')) {
-			strncpy(buf, fname, fe - fname);
-			i = fe - fname;
-		} else {
-			strncpy(buf, fname, strlen(fname));
-			i = strlen(fname);
-			fe = "";
+		if(!reset) {//备份原文件
+			int len;
+			char buf[100], *fe;
+			if(fe = strrchr(fname, '.')) {
+				len = fe - fname;
+				strncpy(buf, fname, len);
+			} else {
+				len = strlen(fname);
+				strncpy(buf, fname, len);
+				fe = "";
+			}
+			int count = 0;
+			do {
+				buf[len] = '\0';
+				sprintf(buf, "%s%s%d%s%s", buf, "(", ++count, ")", fe);
+			} while(!access(buf, F_OK));
+			if(!(fp = fopen(buf, "w"))) { printf("error!\n"); exit(-1); }
+			fwrite(src, sizeof(char), strlen(src), fp);
+			fclose(fp);
 		}
-		int count = 0;
-		do {
-			buf[i] = '\0';
-			sprintf(buf, "%s%s%d%s%s", buf, "(", ++count, ")", fe);//printf("%s\n",buf);
-		} while(!access(buf, F_OK));
-		FILE *fp = fopen(buf, "w");
+		
+		//更新原文件
+		if(!(fp = fopen(fname, "w"))) { printf("error!\n"); exit(-1); }
 		fwrite(emit, sizeof(char), strlen(emit), fp);
 		fclose(fp);
 	}
